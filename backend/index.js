eb4c80ad52e3e33811031a4441f1d94a1a470aa1
Devansh-18 <devanshdubey1827@gmail.com>
2026-04-19 22:57:07 +0530

    Added cors safety

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,12 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://laundary-order-management.vercel.app", "http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // Routes
